0888d9d1e56ed3ba03e37cd9471fb795f722d421
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-02-27 00:16:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+async function start() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://en.wikipedia.org/wiki/JavaScript')
+    await page.screenshot({path: "amazing.png", fullPage: true})
+    await browser.close()
+    
+}
+
+start()
